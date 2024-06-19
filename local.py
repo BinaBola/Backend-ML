cfe0f6284bb730d61545bf0f53de5c20a522b34b
Farhan Al Farisi <130649620+Onyxiaaa@users.noreply.github.com>
2024-06-19 22:56:48 +0700

    Update local.py

--- local.py
+++ local.py
@@ -109,4 +109,4 @@ def predict():
         return jsonify({'error': str(e)}), 400
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=8080)
+    app.run(debug=True, host="127.0.0.1", port=8080)
